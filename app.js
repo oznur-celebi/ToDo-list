2bd8c4339159b94e51996ff880667770d5311fc4
Oznur Celebi <o.ozgumus17@gmail.com>
2021-11-01 11:10:11 +0100

    checkbox added

--- app.js
+++ app.js
@@ -1,74 +1,46 @@
 
-let button = document.querySelector("#add-button");
-let ul =document.querySelector("#mylist");
-    let text = document.querySelector("#input-text");
-button.onclick =()=>{
-    // let ul =document.querySelector("#mylist");
-    // let text = document.querySelector("#input-text");
-    // <i class="fa fa-circle-thin" job ="complete"></i>
-    ul.innerHTML+=`<li class="item">
-    <button class="circle"><i class="fa fa.circle-thin"></i></button>
-    <p class="text"> ${text.value}</p>
-    <button class="btn-trash"><i class="fa fa-trash"></i></button>
-
-</li>`;
-text.value ="";
+let addButton = document.querySelector("#add-button");
+let ul = document.querySelector("#mylist");
+let li = document.querySelectorAll(".item");
+let checkbox = document.querySelectorAll(".item");
+let text = document.querySelector("#input-text");
+let i = 5;
+text.onkeyup = () => {
+    if (text.value != "") {
+        addButton.classList.add("active");
+    } else {
+        addButton.classList.remove('active');
+    }
 }
- ul.addEventListener("click", (e)=>{
-     if (e.target.classList.contains("btn-trash")) {
-         e.target.closest("li").remove();
-     }
-     if (e.target.tagName === 'LI') {
-        e.target.classList.toggle('checked') ;
-     }
- })
+
+addButton.onclick = () => {
+    ul.innerHTML += `<li class="item"><input type="checkbox" name="" class="check" />${text.value}<button class="del"><i
+        class="fa fa-trash delete"></i></button ></li > `;
 
 
+    text.value = "";
+}
 
+ul.addEventListener("click", (e) => {
+    if (e.target.classList.contains("del")) {
+        e.target.closest("li").remove();
+    }
+    if (e.target.classList.contains("delete")){
+        e.target.closest("li").remove();
+    }
 
+    if (e.target.classList.contains("check")) {
+        (e.target.parentElement).classList.toggle('checked');
+    }
+ 
+    })
 
-
-
-
-
-
-
-
-
-
-
-// function addToDo (toDo,id,done) {
-//     const text = `<li class="item">
-//     <i class="fa fa-circle-thin" job ="complete"></i>
-//     <p class="text"> ${toDo}  </p>
-//     <i class="fa fa-trash-o" job ="delete"></i>
-// </li>`;
-//     const position = "beforeend";
-//     list.insertAdjacentHTML(position, text);
-// }
-//     add_button.addEventListener("click", (e)=>{
-//     function addToDo (toDo) {
-//         const text = `<li class="item">
-//         <i class="fa fa-circle-thin" job ="complete"></i>
-//         <p class="text"> ${toDo}  </p>
-//         <i class="fa fa-trash-o" job ="delete"></i>
-//     </li>`;
-//         const position = "beforeend";
-//         list.insertAdjacentHTML(position, text);
-    
-//     }
   
-// });
 
-// the enter key
-// document.addEventListener("keyup", (even)=> {
-//     if(Event.keyCode == 13){
-//         const toDo =input.value;
 
-//         if(toDo){
-//             addToDo(toDo);
-//         }
-//     }
-    
-// })
+
+
+
+
+
 
